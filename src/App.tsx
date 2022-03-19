c437650621482f8b97dfa3760247b0de1a32abd3
gdelgiu <91487859+gdelgiu@users.noreply.github.com>
2022-03-19 03:52:06 +0100

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,47 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <nav className="navbar navbar-dark bg-primary">
-      <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">Navbar</span>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container-fluid">
+          <form className="d-flex">
+            <span className="navbar-brand mb-0 h1">Navbar</span>
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </form>
+        </div>
+      </nav>
+      <nav aria-label="...">
+        <ul className="pagination">
+          <li className="page-item disabled">
+            <a className="page-link">Previous</a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li className="page-item active" aria-current="page">
+            <a className="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
