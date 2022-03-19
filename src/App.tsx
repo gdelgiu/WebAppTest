74060ef56282ab56b2c7cd348b2d95a969568541
gdelgiu <91487859+gdelgiu@users.noreply.github.com>
2022-03-19 02:53:42 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">Navbar</span>
+      </div>
+    </nav>
+  );
+}
